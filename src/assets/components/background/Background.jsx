b1f72bdfd0302c2230b7340fab5f023080a429b5
chixx <lucottejeremie@gmail.com>
2023-07-14 11:10:18 +0200

    Adding dist

--- src/assets/components/background/Background.jsx
+++
@@ -1,9 +0,0 @@
-export const Background = ({ children }) => {
-  return (
-    <div>
-      <div className="bg-[url('/img/background.png')] bg-no-repeat bg-cover h-screen w-screen">
-        {children}
-      </div>
-    </div>
-  );
-};
